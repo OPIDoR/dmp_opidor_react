b4a973b0a631d2df92adc16587f748305ac7d4ef
BRAHMI <brahmi@ads.intra.inist.fr>
2023-04-19 09:51:37 +0200

    update margin search product

--- src/components/SearchProduct/SearchProduct.jsx
+++ src/components/SearchProduct/SearchProduct.jsx
@@ -10,21 +10,18 @@ import { Panel, PanelGroup } from "react-bootstrap";
 import { TfiAngleDown } from "react-icons/tfi";
 import { TfiAngleUp } from "react-icons/tfi";
 import CustumButton from "../Styled/CustumButton";
+import stylesSidebar from "../assets/css/sidebar.module.css";
 import { useContext } from "react";
 import { GlobalContext } from "../context/Global";
 
 const ProductStyle = styled.div`
   color: #000;
   padding: 0px;
-  margin: 4px 0px 4px 230px;
+  margin: 4px -145px 4px 75px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  //   border-radius: 8px 8px 8px 8px;
-  //   border: solid;
-  //   border-color: var(--primary);
-  //   border-width: 1px;
-  padding: 35px;
+  padding: 20px 35px 20px 35px;
 `;
 function SearchProduct({ planId }) {
   const { setProductData } = useContext(GlobalContext);
@@ -68,76 +65,80 @@ function SearchProduct({ planId }) {
   };
 
   return (
-    <ProductStyle>
-      <PanelGroup accordion id="accordion-example">
-        <Panel eventKey="1" style={{ borderRadius: "10px", borderWidth: "2px", borderColor: "var(--primary)" }}>
-          <Panel.Heading style={{ background: "white", borderRadius: "18px" }}>
-            <Panel.Title toggle>
-              <div className={styles.question_title}>
-                <div className={styles.question_text}>
-                  <p className={styles.title}>Produit de recherche</p>
-                </div>
-                <span>
-                  {/* 3 */}
-                  {true ? (
-                    <TfiAngleDown style={{ minWidth: "35px" }} size={35} className={styles.down_icon} />
-                  ) : (
-                    <TfiAngleUp size={35} style={{ minWidth: "35px" }} className={styles.down_icon} />
-                  )}
-                </span>
-              </div>
-            </Panel.Title>
-          </Panel.Heading>
+    <div className={stylesSidebar.section}>
+      <div className={stylesSidebar.main}>
+        <ProductStyle>
+          <PanelGroup accordion id="accordion-example">
+            <Panel eventKey="1" style={{ borderRadius: "10px", borderWidth: "2px", borderColor: "var(--primary)" }}>
+              <Panel.Heading style={{ background: "white", borderRadius: "18px" }}>
+                <Panel.Title toggle>
+                  <div className={styles.question_title}>
+                    <div className={styles.question_text}>
+                      <p className={styles.title}>Produit de recherche</p>
+                    </div>
+                    <span>
+                      {/* 3 */}
+                      {true ? (
+                        <TfiAngleDown style={{ minWidth: "35px" }} size={35} className={styles.down_icon} />
+                      ) : (
+                        <TfiAngleUp size={35} style={{ minWidth: "35px" }} className={styles.down_icon} />
+                      )}
+                    </span>
+                  </div>
+                </Panel.Title>
+              </Panel.Heading>
 
-          <Panel.Body collapsible={true}>
-            <div style={{ margin: "25px" }}>
-              <div className="form-group">
-                <div className={stylesForm.label_form}>
-                  <strong className={stylesForm.dot_label}></strong>
-                  <label>Abbreviation</label>
+              <Panel.Body collapsible={true}>
+                <div style={{ margin: "25px" }}>
+                  <div className="form-group">
+                    <div className={stylesForm.label_form}>
+                      <strong className={stylesForm.dot_label}></strong>
+                      <label>Abbreviation</label>
+                    </div>
+                    <input
+                      value={abbreviation}
+                      className={`form-control ${stylesForm.input_text}`}
+                      placeholder={"ajouter abbreviation"}
+                      type="text"
+                      onChange={(e) => setAbbreviation(e.target.value)}
+                    />
+                    <small className="form-text text-muted">Limité à 20 caractères</small>
+                  </div>
+                  <div className="form-group">
+                    <div className={stylesForm.label_form}>
+                      <strong className={stylesForm.dot_label}></strong>
+                      <label>Title</label>
+                    </div>
+                    <input
+                      value={title}
+                      className={`form-control ${stylesForm.input_text}`}
+                      placeholder={"ajouter titre"}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <div className={stylesForm.label_form}>
+                      <strong className={stylesForm.dot_label}></strong>
+                      <label>Type</label>
+                    </div>
+                    {data && (
+                      <Select
+                        menuPortalTarget={document.body}
+                        styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999, color: "grey" }) }}
+                        options={data}
+                        style={{ color: "red" }}
+                        onChange={handleSelect}
+                      />
+                    )}
+                  </div>
+                  <CustumButton title="Ajouter" position="start" type={"primary"} handleClick={handleSave}></CustumButton>
                 </div>
-                <input
-                  value={abbreviation}
-                  className={`form-control ${stylesForm.input_text}`}
-                  placeholder={"ajouter abbreviation"}
-                  type="text"
-                  onChange={(e) => setAbbreviation(e.target.value)}
-                />
-                <small className="form-text text-muted">Limité à 20 caractères</small>
-              </div>
-              <div className="form-group">
-                <div className={stylesForm.label_form}>
-                  <strong className={stylesForm.dot_label}></strong>
-                  <label>Title</label>
-                </div>
-                <input
-                  value={title}
-                  className={`form-control ${stylesForm.input_text}`}
-                  placeholder={"ajouter titre"}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
-              </div>
-              <div className="form-group">
-                <div className={stylesForm.label_form}>
-                  <strong className={stylesForm.dot_label}></strong>
-                  <label>Type</label>
-                </div>
-                {data && (
-                  <Select
-                    menuPortalTarget={document.body}
-                    styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999, color: "grey" }) }}
-                    options={data}
-                    style={{ color: "red" }}
-                    onChange={handleSelect}
-                  />
-                )}
-              </div>
-              <CustumButton title="Ajouter" position="start" type={"primary"} handleClick={handleSave}></CustumButton>
-            </div>
-          </Panel.Body>
-        </Panel>
-      </PanelGroup>
-    </ProductStyle>
+              </Panel.Body>
+            </Panel>
+          </PanelGroup>
+        </ProductStyle>
+      </div>
+    </div>
   );
 }
 
