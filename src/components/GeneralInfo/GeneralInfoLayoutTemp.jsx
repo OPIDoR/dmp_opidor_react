b582afd01357c32d9efafe5ab0c59bdc47b42b39
Benjamin FAURE <benjamin.faure@inist.fr>
2023-06-06 14:54:15 +0200

    Temporary renaming of files befire merge with developmement

---
+++ src/components/GeneralInfo/GeneralInfoLayoutTemp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Global from '../context/Global.jsx';
+import GeneralInfo from './GeneralInfo.jsx';
+import '../../i18n.js';
+
+
+function GeneralInfoLayout({ planId, dmpId, projectFragmentId, metaFragmentId, locale = 'en_GB', isTestPlan = false }) {
+
+  return(
+    <Global>
+      <GeneralInfo
+        locale={locale}
+        planId={planId}
+        dmpId={dmpId}
+        projectFragmentId={projectFragmentId}
+        metaFragmentId={metaFragmentId}
+        isTestPlan={isTestPlan}
+      />
+    </Global>
+  )
+}
+
+export default GeneralInfoLayout;
