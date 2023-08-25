464ef6a82f35b2bf33b37dd42441067908a3f0ec
Benjamin FAURE <benjamin.faure@inist.fr>
2023-08-25 13:49:19 +0200

    Improved display of modal fragments

--- src/components/Forms/ModalTemplate.jsx
+++ src/components/Forms/ModalTemplate.jsx
@@ -162,7 +162,7 @@ function ModalTemplate({
           )}
         </div>
         {fragmentsList && template && (
-          <table style={{ marginTop: "20px" }} className="table">
+          <table style={{ marginTop: "20px" }} className="table table-hover">
             <thead>
               {fragmentsList.length > 0 &&
                 template &&
@@ -170,6 +170,7 @@ function ModalTemplate({
                 fragmentsList.some((el) => el.action !== "delete") && (
                   <tr>
                     <th scope="col">{header}</th>
+                    <th scope="col">Actions</th>
                   </tr>
                 )}
             </thead>
@@ -178,43 +179,25 @@ function ModalTemplate({
                 .filter((el) => el.action !== "delete")
                 .map((el, idx) => (
                   <tr key={idx}>
-                    <td scope="row" style={{ width: "100%" }}>
-                      <div className={styles.border}>
-                        <div className={styles.panel_title} dangerouslySetInnerHTML={createMarkup(parsePattern(el, template.to_string))}></div>
-                        {!readonly && (
-                          <div className={styles.table_container}>
-                            <div className="col-md-1">
-                              {level === 1 && (
-                                <span>
-                                  <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
-                                    <i className="fa fa-pen-to-square" />
-                                  </a>
-                                </span>
-                              )}
-                            </div>
-                            <div className="col-md-1">
-                              <span>
-                                <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleDeleteList(e, idx)}>
-                                  <i className="fa fa-times" />
-                                </a>
-                              </span>
-                            </div>
-                          </div>
-                        )}
-                        {readonly && (
-                          <div className={styles.table_container}>
-                            <div className="col-md-1">
-                              {level === 1 && (
-                                <span style={{ marginRight: "10px" }}>
-                                  <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
-                                    <i className="fa fa-eye" />
-                                  </a>
-                                </span>
-                              )}
-                            </div>
-                          </div>
-                        )}
-                      </div>
+                    <td scope="row" style={{ width: "100%" }} dangerouslySetInnerHTML={createMarkup(parsePattern(el, template.to_string))}></td>
+                    <td className="actions">
+                      {!readonly && (
+                        <>
+                          {level === 1 && (
+                            <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
+                              <i className="fa fa-pen-to-square" />
+                            </a>
+                          )}
+                          <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleDeleteList(e, idx)}>
+                            <i className="fa fa-times" />
+                          </a>
+                        </>
+                      )}
+                      {readonly && level === 1 && (
+                        <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
+                          <i className="fa fa-eye" />
+                        </a>
+                      )}
                     </td>
                   </tr>
                 ))}
