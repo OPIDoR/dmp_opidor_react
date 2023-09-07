17b6d3383022dbd5408083467019c43914708432
Benjamin FAURE <benjamin.faure@inist.fr>
2023-09-07 15:41:36 +0200

    Removed unused components & fixed some imports

--- src/components/Shared/index.jsx
+++ src/components/Shared/index.jsx
@@ -2,16 +2,9 @@ import Banner from "./Banner";
 import CustomError from "./CustomError";
 import CustomSelect from "./CustomSelect";
 import CustomSpinner from "./CustomSpinner";
-import Footer from "./Footer";
-import Header from "./Header";
-import Navbar from "./Navbar";
 
 export {
-  Banner,
   CustomError,
   CustomSelect,
   CustomSpinner,
-  Footer,
-  Header,
-  Navbar,
 };
