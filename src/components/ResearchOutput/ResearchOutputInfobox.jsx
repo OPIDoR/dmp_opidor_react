74f816c34012ae111ef2f50510aa7a88a6a4f7b1
Benjamin FAURE <benjamin.faure@inist.fr>
2023-09-19 16:17:42 +0200

    feat: refactored research output infobox into it's own component

---
+++ src/components/ResearchOutput/ResearchOutputInfobox.jsx
@@ -0,0 +1,95 @@
+import React, { useContext } from "react";
+import styles from "../assets/css/write_plan.module.css";
+import { Panel } from "react-bootstrap";
+import { Tooltip } from "react-tooltip";
+import { AiOutlineEdit } from "react-icons/ai";
+import { FaTrash } from "react-icons/fa6";
+import { GlobalContext } from "../context/Global";
+import { useTranslation } from "react-i18next";
+import PanelBody from "react-bootstrap/lib/PanelBody";
+
+
+function ResearchOutputInfobox({ handleEdit, handleDelete, readonly }) {
+  const { t } = useTranslation();
+  const {
+    displayedResearchOutput,
+  } = useContext(GlobalContext);
+
+
+  return (
+    <Panel
+      className={styles.panel}
+      style={{
+        borderRadius: "10px",
+        borderWidth: "2px",
+        borderColor: "var(--primary)",
+      }}
+    >
+      <Panel.Heading style={{
+        backgroundColor: "rgb(28, 81, 112)",
+        borderRadius: "5px 5px 0 0",
+        color: "#fff",
+      }}>
+        <Panel.Title style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}>
+          <strong>{displayedResearchOutput?.title}</strong>
+          <span id="actions" style={{ display: "flex" }}>
+            {!readonly && (
+              <>
+                <Tooltip anchorSelect="#editBtn" place="bottom">
+                  {t("Edit")}
+                </Tooltip>
+                <button
+                  type="button"
+                  className="btn btn-link btn-sm m-0 p-0"
+                  style={{ outline: "none", color: "#fff", padding: 0, margin: "2px 5px 0 5px" }}
+                  onClick={handleEdit}
+                  id="editBtn"
+                >
+                  <AiOutlineEdit size={22} />
+                </button>
+              </>
+            )}
+            {!readonly && displayedResearchOutput.order !== 1 && (
+              <>
+                <Tooltip anchorSelect="#deleteBtn" place="bottom">
+                  {t("Delete")}
+                </Tooltip>
+                <button
+                  type="button"
+                  className="btn btn-link btn-sm m-0 p-0"
+                  style={{ outline: "none", color: "#fff", padding: 0, margin: "2px 5px 0 5px" }}
+                  onClick={handleDelete}
+                  id="deleteBtn"
+                >
+                  <FaTrash size={22} />
+                </button>
+              </>
+            )}
+          </span>
+        </Panel.Title>
+      </Panel.Heading>
+      <PanelBody>
+        <ul>
+          <li>
+            {t("Title")}: <strong>{displayedResearchOutput.title}</strong>
+          </li>
+          <li>
+            {t("Research Output Name")}: <strong>{displayedResearchOutput.abbreviation}</strong>
+          </li>
+          <li>
+            {t("Contains personal data")}: <strong>{displayedResearchOutput.hasPersonalData ? t("Yes") : t("No")}</strong>
+          </li>
+          <li>
+            {t("Type")}: <strong>{t(displayedResearchOutput.type || '-')}</strong>
+          </li>
+        </ul>
+      </PanelBody>
+    </Panel>
+  );
+}
+
+export default ResearchOutputInfobox;
