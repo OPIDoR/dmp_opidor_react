d7c64b41a03b77a0ab9b74f9292276e174f669b3
wilmouths <wilmouthsteven@gmail.com>
2023-10-04 16:07:02 +0200

    refactor: hide table header if no header defined

--- src/components/FormComponents/FragmentList.jsx
+++ src/components/FormComponents/FragmentList.jsx
@@ -1,43 +1,55 @@
 import React from 'react';
+import { FaEdit, FaEye } from 'react-icons/fa';
+import { IoIosClose } from 'react-icons/io';
 import { createMarkup, parsePattern } from '../../utils/GeneratorUtils';
 
 function FragmentList({ fragmentsList, handleEdit, handleDelete, parent = 'form', templateToString = [], tableHeader = null, readonly = false }) {
   return (
     <>
       {fragmentsList  && (
-        <table style={{ marginTop: "20px" }} className="table table-hover">
-          <thead>
-            {fragmentsList.length > 0 &&
-              fragmentsList.some((el) => el.action !== "delete") && (
-                <tr>
-                  <th scope="col">{tableHeader}</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              )}
+        <table style={{ marginTop: '20px' }} className="table table-hover">
+          {tableHeader && (
+            <thead>
+              {fragmentsList.length > 0 &&
+                fragmentsList.some((el) => el.action !== "delete") && (
+                  <tr>
+                    <th scope="col">{tableHeader}</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                )}
           </thead>
+          )}
           <tbody>
             {fragmentsList
-              .map((el, idx) => (el.action !== "delete" ?
+              .map((el, idx) => (el.action !== 'delete' ?
                 <tr key={idx}>
-                  <td style={{ width: "100%" }} dangerouslySetInnerHTML={createMarkup(parsePattern(el, templateToString))}></td>
-                  <td className="actions">
-                    {!readonly && (
-                      <>
-                        {parent === 'form' && (
-                          <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
-                            <i className="fa fa-pen-to-square" />
-                          </a>
-                        )}
-                        <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleDelete(e, idx)}>
-                          <i className="fa fa-xmark" />
-                        </a>
-                      </>
-                    )}
-                    {readonly && parent === 'form' && (
-                      <a className="text-primary" href="#" aria-hidden="true" onClick={(e) => handleEdit(e, idx)}>
-                        <i className="fa fa-eye" />
-                      </a>
-                    )}
+                  <td style={{ width: '100%', border: '1px solid #1C5170' }} dangerouslySetInnerHTML={createMarkup(parsePattern(el, templateToString))}></td>
+                  <td style={{ border: '1px solid #1C5170' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+                      {!readonly && (
+                        <>
+                          {parent === 'form' && (
+                            <FaEdit
+                              size={18}
+                              onClick={() => handleEdit(idx)}
+                              style={{ cursor: 'pointer', margin: '0 2px 0 2px' }}
+                            />
+                          )}
+                          <IoIosClose
+                            onClick={(e) => handleDelete(idx)}
+                            size={18}
+                            style={{cursor: 'pointer', margin: '0 2px 0 2px'  }}
+                          />
+                        </>
+                      )}
+                      {readonly && parent === 'form' && (
+                        <FaEye
+                          size={18}
+                          onClick={() => handleEdit(idx)}
+                          style={{ cursor: 'pointer', margin: '0 2px 0 2px' }}
+                        />
+                      )}
+                    </div>
                   </td>
                 </tr> : null
               ))}
