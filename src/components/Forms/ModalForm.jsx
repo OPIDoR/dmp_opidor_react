cdfb0269a58ac90ab789c37c18f7c50e9c0a25aa
Benjamin FAURE <benjamin.faure@inist.fr>
2023-10-23 14:11:42 +0200

    feat: added generic ModalForm component

---
+++ src/components/Forms/ModalForm.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import FormBuilder from './FormBuilder';
+import { useTranslation } from 'react-i18next';
+
+
+function ModalForm({ fragmentId, data, template, label, readonly, level, show, handleSave, handleClose }) {
+  const { t } = useTranslation();
+  const [modalData, setModalData] = useState({});
+
+  const handleModalValueChange = (propName, value) => {
+    setModalData({ ...modalData, [propName]: value });
+  }
+  useEffect(() => {
+    setModalData(data);
+  }, [data]);
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header>
+        <Modal.Title style={{ color: "var(--orange)", fontWeight: "bold" }}>{label}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ padding: "20px !important" }}>
+        <FormBuilder
+          fragment={modalData}
+          handleChangeValue={handleModalValueChange}
+          template={template}
+          level={level + 1}
+          fragmentId={fragmentId}
+          readonly={readonly}
+        ></FormBuilder>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          {t("Close")}
+        </Button>
+        {!readonly && (
+          <Button variant="primary" onClick={() => handleSave(modalData)}>
+            {t("Save")}
+          </Button>
+        )}
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default ModalForm;
