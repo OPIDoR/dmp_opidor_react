82fcad3634cfb2d959af556d784c33d5a5f47a72
wilmouths <wilmouthsteven@gmail.com>
2023-12-08 10:54:05 +0100

    refactor: update funders data

--- src/services/generalInfo.js
+++ src/services/generalInfo.js
@@ -10,10 +10,8 @@ const dataFundingOrganization = [
       fr: 'Agence Nationale de la Recherche (ANR)',
       en: 'French National Research Agency (NRA)',
     },
-    script: {
-      id: 1,
-      name: 'AnrProject',
-    },
+    scriptName: 'Anr_data_fetcher_v3',
+    registry: 'AnrProjects',
   },
   {
     id: 2,
@@ -21,22 +19,12 @@ const dataFundingOrganization = [
       fr: 'Commission Européenne',
       en: 'European Commission',
     },
-    script: {
-      id: 2,
-      name: 'CordisProject',
-    },
+    scriptName: 'Cordis_data_fetcher',
+    registry: 'CordisProjects',
   },
 ];
 
-/**
- * The function "getComments" returns a mock data object for comments.
- * @param t - It is likely that "t" is a parameter representing the "test" object or module used for testing the function. It is common to use "t" as a
- * shorthand for "test" in testing frameworks such as Jest or Mocha.
- * @param token - The `token` parameter is likely an authentication token that is used to authenticate the user making the request to the server. It is
- * usually obtained after the user logs in and is used to verify the user's identity for subsequent requests.
- * @returns An object with a "data" property that contains the value of the "dataComent" variable.
- */
-const getFunders = async (token) => ({ data: dataFundingOrganization });
+const getFunders = async () => ({ data: dataFundingOrganization });
 
 const saveIsTestPlan = async (planId, isTestPlan) => 
   axios.post(`/plans/${planId}/set_test`, { is_test: isTestPlan }, { headers: commonHeaders });
@@ -46,10 +34,11 @@ const saveIsTestPlan = async (planId, isTestPlan) =>
  * @param grantId - The ID of a grant project.
  * @param projectFragmentId - The `projectFragmentId` parameter is an identifier for a specific fragment of a project. It is used in conjunction with the
  * `grantId` parameter to retrieve data related to a specific project and fragment.
+ * @param scriptName Script name
  * @returns An object with a "data" property that contains an empty array.
  */
-const saveFunder = async (grantId, projectFragmentId, scriptId) => 
-  axios.get(`/codebase/project_search?project_id=${grantId}&fragment_id=${projectFragmentId}&script_id=${scriptId}`);
+const saveFunder = async (grantId, projectFragmentId, scriptName) =>
+  axios.get(`/codebase/project_search?project_id=${grantId}&fragment_id=${projectFragmentId}&script_name=${scriptName}`);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
