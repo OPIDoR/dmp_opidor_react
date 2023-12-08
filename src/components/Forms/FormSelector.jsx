e0041f1305cb0a0ef29126a09a20ed1167f636d6
Benjamin FAURE <benjamin.faure@inist.fr>
2023-12-08 16:16:10 +0100

    feat: change form section is now toggeable

--- src/components/Forms/FormSelector.jsx
+++ src/components/Forms/FormSelector.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { FaShuffle } from "react-icons/fa6";
+import { Button } from 'react-bootstrap';
 
 import styles from '../assets/css/form_selector.module.css';
 import CustomSpinner from '../Shared/CustomSpinner';
@@ -10,6 +13,7 @@ import CustomSelect from "../Shared/CustomSelect";
 function FormSelector({ className, selectedTemplateId, fragmentId, setFragment, setTemplate }) {
   const { t } = useTranslation();
   const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(false);
   const [availableTemplates, setAvailableTemplates] = useState([]);
   const [selectedTemplate, setSelectedTemplate] = useState(null);
   const {
@@ -30,32 +34,72 @@ function FormSelector({ className, selectedTemplateId, fragmentId, setFragment, 
 
   const handleSelectTemplate = (e) => {
     setSelectedTemplate(e.object);
+  }
+
+  const handleChangeForm = () => {
     setLoading(true)
-    service.changeForm(fragmentId, e.value).then((res) => {
+    service.changeForm(fragmentId, selectedTemplate.id).then((res) => {
       setFragment(res.data.fragment);
       setTemplate(res.data.template)
     }).catch(console.error)
-      .finally(() => setLoading(false));
+      .finally(() => {
+        setShow(false);
+        setLoading(false);
+      });
   }
+
   return (
-    <div className={styles.container} style={{ position: 'relative' }}>
-      {loading && (<CustomSpinner isOverlay={true}></CustomSpinner>)}
-      {availableTemplates.length > 1 && selectedTemplate && (
+    <>
+      {availableTemplates.length > 1 && (
         <>
-          <label htmlFor={className} className={styles.label}>{t('You can pick a different question form')}</label>
-          <CustomSelect
-            propName={className}
-            onSelectChange={handleSelectTemplate}
-            options={availableTemplates.map((tplt) => ({
-              value: tplt.id,
-              label: tplt.label,
-              object: tplt,
-            }))}
-            selectedOption={selectedTemplate}
-          />
+          {show && (
+            <div className={styles.container} style={{ position: 'relative' }}>
+              {selectedTemplate && (
+                <>
+                  {loading && (<CustomSpinner isOverlay={true}></CustomSpinner>)}
+                  <label htmlFor={className} className={styles.label}>{t('You can pick a different question form')}</label>
+                  <CustomSelect
+                    propName={className}
+                    onSelectChange={handleSelectTemplate}
+                    options={availableTemplates.map((tplt) => ({
+                      value: tplt.id,
+                      label: tplt.label,
+                      object: tplt,
+                    }))}
+                    selectedOption={selectedTemplate}
+                  />
+                  <div className={styles.form_selector_footer}>
+                    <Button onClick={() => setShow(false)} style={{ margin: '0 5px 0 5px' }}>
+                      {t("Cancel")}
+                    </Button>
+                    <Button onClick={handleChangeForm} bsStyle="primary" type="submit" style={{ margin: '0 5px 0 5px' }}>
+                      {t('Save')}
+                    </Button>
+                  </div>
+                </>
+              )}
+            </div>
+          )}
+          {!show && (
+            <div className={styles.form_change_toggle}>
+              <ReactTooltip
+                id="form-change-show-button"
+                place="bottom"
+                effect="solid"
+                variant="info"
+                content={t('Change form')}
+              />
+              <FaShuffle
+                data-tooltip-id="form-change-show-button"
+                size={18}
+                onClick={() => setShow(true)}
+                style={{ cursor: 'pointer', margin: '0 2px 0 2px' }}
+              />
+            </div>
+          )}
         </>
       )}
-    </div>
+    </>
   )
 }
 
