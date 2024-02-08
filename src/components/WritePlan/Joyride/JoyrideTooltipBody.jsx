714fb8c9097a84d3a5a7bd48d18f8d29e06c493d
wilmouths <wilmouthsteven@gmail.com>
2024-02-08 11:01:23 +0100

    feat: create Joyride components

---
+++ src/components/WritePlan/Joyride/JoyrideTooltipBody.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const bodyStyle = {
+  color: 'var(--dark-blue)',
+  padding: '10px',
+  borderRadius: '10px',
+  boxSizing: 'border-box',
+  alignItems: 'center',
+  background: 'var(--white)',
+  minHeight: '200px',
+};
+
+function JoyrideTooltipBody({
+  className,
+  style,
+  children,
+}) {
+  return (
+    <div
+      style={{ ...bodyStyle, ...style }}
+      className={className}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default JoyrideTooltipBody;
