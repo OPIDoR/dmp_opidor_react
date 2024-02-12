5873bb3e62a318707fb03de45f8415c11a069f22
wilmouths <wilmouthsteven@gmail.com>
2024-02-12 09:49:39 +0100

    refactor: updating guided tour

--- src/components/WritePlan/WritePlanLayout.jsx
+++ src/components/WritePlan/WritePlanLayout.jsx
@@ -1,12 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
-import Joyride, { STATUS, EVENTS } from 'react-joyride';
 
 import Global from '../context/Global.jsx';
 import WritePlan from './WritePlan.jsx'
-import JoyrideTooltip from './Joyride/JoyrideTooltip.jsx';
+import Joyride from './Joyride/Joyride.jsx';
 import '../../i18n.js';
-import JoyrideBeacon from './Joyride/JoyrideBeacon.jsx';
 
 const toastOptions = {
   duration: 5000,
@@ -22,122 +20,38 @@ function WritePlanLayout({
   readonly,
 }) {
 
-  const localeSteps = {
-    skip: 'Ignorer la visite guidée',
-    back: 'Précédent',
-    next: 'Suivant',
-    close: 'Fermer',
-    last: 'Terminer',
-  };
-  const title = 'Découvrez les fonctionnalités de rédaction';
+  const steps = [
+    {
+      title: 'Découvrez les fonctionnalités de rédaction',
+      content: (
+        <>
+          Je suis capable du meilleur et du pire. Mais dans le pire, c'est moi le meilleur.
+          <img src="https://picsum.photos/300/200" alt="Image" />
+        </>
+      ),
+      placement: 'center',
+      target: 'body',
+    },
+    {
+      title: 'Découvrez ou sont les commentaires',
+      content: 'Ce n\'est pas parce qu\'ils sont nombreux à avoir tort qu\'ils ont forcément raison.',
+      placementBeacon: 'top',
+      target: '.research-outputs-tabs',
+    },
+  ];
 
-  const [steps, setSteps] = useState({
-    run: true,
-    steps: [
-      {
-        title,
-        content: (
-          <>
-            Je suis capable du meilleur et du pire. Mais dans le pire, c'est moi le meilleur.
-            <img src="https://picsum.photos/300/200" alt="Image" />
-          </>
-        ),
-        locale: localeSteps,
-        placement: 'center',
-        target: 'body',
-      },
-      {
-        title,
-        content: 'Ce n\'est pas parce qu\'ils sont nombreux à avoir tort qu\'ils ont forcément raison.',
-        locale: localeSteps,
-        target: '.research-outputs-tabs',
-      },
-    ],
-  });
+  const [run, setRunState] = useState(false);
 
-  console.log('ATOTOTOTOTOTO')
-
-  const joyrideRef = useRef('joyride-ref');
-
-  const handleClickStart = (event) => {
+  const setRun = (event) => {
     event.preventDefault();
 
-    setSteps({
-      run: true,
-      steps: steps.steps,
-    });
-  };
-
-  const handleJoyrideCallback = (data) => {
-    const { status } = data;
-    const finishedStatuses = [STATUS.FINISHED, STATUS.SKIPPED, EVENTS.TARGET_NOT_FOUND];
-
-    if (finishedStatuses.includes(status)) {
-      setSteps({
-        run: false,
-        steps: steps.steps,
-      });
-    }
-  };
-
-  const joyrideTooltip = (props) => {
-    const {
-      backProps,
-      continuous,
-      index,
-      primaryProps,
-      skipProps,
-      step,
-      size,
-    } = props;
-
-    return (
-      <JoyrideTooltip {...props } ref={joyrideRef}>
-        <JoyrideTooltip.Header index={index} size={size}>
-          <JoyrideTooltip.Title>
-            {step?.title && (<>{step.title}</>) }
-          </JoyrideTooltip.Title>
-        </JoyrideTooltip.Header>
-
-        <JoyrideTooltip.Body>
-          {step.content}
-        </JoyrideTooltip.Body>
-
-        <JoyrideTooltip.Footer>
-          {skipProps && <JoyrideTooltip.Button { ...skipProps } type="link" style={{ width: '90px' }} />}
-          <JoyrideTooltip.Spacer />
-          <div>
-            {backProps && index > 0 && (
-              <JoyrideTooltip.Button { ...backProps } style={{ marginRight: '10px' }} />
-            )}
-            {primaryProps && <JoyrideTooltip.Button { ...primaryProps } title={continuous ? primaryProps.title : skipProps.title} />}
-          </div>
-        </JoyrideTooltip.Footer>
-      </JoyrideTooltip>
-    );
+    setRunState(true);
   }
 
   return(
     <Global>
-      <Joyride
-        callback={handleJoyrideCallback}
-        continuous
-        hideCloseButton
-        run={steps.run}
-        scrollToFirstStep
-        showProgress
-        showSkipButton
-        steps={steps.steps}
-        styles={{
-          options: {
-            zIndex: 10000,
-            arrowColor: 'var(--rust)',
-          },
-        }}
-        // beaconComponent={(props) => <JoyrideBeacon { ...props } ref={joyrideRef} />}
-        // tooltipComponent={joyrideTooltip}
-      />
-      <button onClick={(e) => handleClickStart(e)}>Visite guidée</button>
+      <Joyride tourName="write_plan" steps={steps} locale={locale} run={run} setRunState={setRunState} />
+      <button onClick={setRun}>Run</button>
       <WritePlan
         planId={planId}
         templateId={templateId}
@@ -146,6 +60,7 @@ function WritePlanLayout({
         currentOrgId={currentOrgId}
         currentOrgName={currentOrgName}
         readonly={readonly}
+        className="research-outputs-tabs"
       />
       <Toaster position="top-center" toastOptions={toastOptions} reverseOrder={false} />
     </Global>
