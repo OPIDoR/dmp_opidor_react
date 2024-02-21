60e42d7e0e1d456817a426b6b9795be7382777ad
wilmouths <wilmouthsteven@gmail.com>
2024-02-21 18:24:30 +0100

    refactor: lint i18n file

--- src/i18nTest.js
+++ src/i18nTest.js
@@ -1,12 +1,14 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(initReactI18next) // pass the i18n instance to react-i18next.
   .init({
-    resources: {}, // resources would be your translations. This can be an empty object for testing if you don't want to use actual translations.
-    lng: "fr", // language to use
-    fallbackLng: "fr",
+    // eslint-disable-next-line max-len
+    // resources would be your translations. Can be an empty object for testing if you don't want to use actual translations.
+    resources: {},
+    lng: 'fr', // language to use
+    fallbackLng: 'fr',
 
     interpolation: {
       escapeValue: false, // not needed for React
