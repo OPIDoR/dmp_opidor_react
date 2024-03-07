d1da07afbc71e3d6261d83246cfcbd17c587021b
wilmouths <wilmouthsteven@gmail.com>
2024-03-07 10:28:43 +0100

    refactor: adding reactour

--- src/components/WritePlan/WritePlanLayout.jsx
+++ src/components/WritePlan/WritePlanLayout.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import { TourProvider } from '@reactour/tour'
 
 import Global from '../context/Global.jsx';
 import WritePlan from './WritePlan.jsx'
-import Joyride from './Joyride/Joyride.jsx';
 import '../../i18n.js';
+
+import steps from '../Shared/Tours/WritePlan.jsx';
 
 const toastOptions = {
   duration: 5000,
@@ -19,49 +21,27 @@ function WritePlanLayout({
   currentOrgName,
   readonly,
 }) {
-
-  const steps = [
-    {
-      title: 'Découvrez les fonctionnalités de rédaction',
-      content: (
-        <>
-          Je suis capable du meilleur et du pire. Mais dans le pire, c'est moi le meilleur.
-          <img src="https://picsum.photos/300/200" alt="Image" />
-        </>
-      ),
-      placement: 'center',
-      target: 'body',
-    },
-    {
-      title: 'Découvrez ou sont les commentaires',
-      content: 'Ce n\'est pas parce qu\'ils sont nombreux à avoir tort qu\'ils ont forcément raison.',
-      placementBeacon: 'top',
-      target: '.research-outputs-tabs',
-    },
-  ];
-
-  const [run, setRunState] = useState(false);
-
-  const setRun = (event) => {
-    event.preventDefault();
-
-    setRunState(true);
-  }
-
   return(
     <Global>
-      <Joyride tourName="write_plan" steps={steps} locale={locale} run={run} setRunState={setRunState} />
-      <button onClick={setRun}>Run</button>
-      <WritePlan
-        planId={planId}
-        templateId={templateId}
-        locale={locale}
-        userId={userId}
-        currentOrgId={currentOrgId}
-        currentOrgName={currentOrgName}
-        readonly={readonly}
-        className="research-outputs-tabs"
-      />
+      {/* <Joyride tourName="write_plan" steps={steps} locale={locale} run={run} setRunState={setRunState} />
+      <button onClick={setRun}>Run</button> */}
+      <TourProvider
+        steps={steps}
+        scrollSmooth={true}
+        badgeContent={({ currentStep, totalSteps }) => `${currentStep + 1} / ${totalSteps}`}
+        onClickClose={(props) => {}}
+      >
+        <WritePlan
+          planId={planId}
+          templateId={templateId}
+          locale={locale}
+          userId={userId}
+          currentOrgId={currentOrgId}
+          currentOrgName={currentOrgName}
+          readonly={readonly}
+          className="research-outputs-tabs"
+        />
+      </TourProvider>
       <Toaster position="top-center" toastOptions={toastOptions} reverseOrder={false} />
     </Global>
   )
