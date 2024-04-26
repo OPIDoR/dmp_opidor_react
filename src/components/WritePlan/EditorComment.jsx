397cf43b6369802c5884b53c3157c367bdcaec1f
wilmouths <wilmouthsteven@gmail.com>
2024-04-26 14:10:16 +0200

    lint: adding space between function and return

--- src/components/WritePlan/EditorComment.jsx
+++ src/components/WritePlan/EditorComment.jsx
@@ -8,6 +8,7 @@ const EditorComment = ({ initialValue, updateParentText }) => {
   const handleEditorChange = (content) => {
     updateParentText(content);
   };
+
   return (
     <Editor
       onEditorChange={handleEditorChange}
