a557719fc897ec5e7a43d1ff60fbf299343a6841
Hugo COLLIN <hugo.collin@inist.fr>
2024-05-14 15:32:16 +0200

    fix: solving PlanCreation error by passing SectionsMappingProvider

--- src/components/PlanCreation/PlanCreationLayout.jsx
+++ src/components/PlanCreation/PlanCreationLayout.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import '../../i18n';
 
 import Global from '../context/Global.jsx';
 import PlanCreation from './PlanCreation.jsx';
-import '../../i18n';
+import { SectionsMappingProvider } from '../context/SectionsMappingContext.jsx';
 
 const queryClient = new QueryClient();
 
 function PlanCreationLayout({ locale, currentOrgId, currentOrgName }) {
   return (
     <Global>
-      <QueryClientProvider client={queryClient}>
-        <PlanCreation
-          locale={locale}
-          currentOrgId={currentOrgId}
-          currentOrgName={currentOrgName}
-        />
-      </QueryClientProvider>
+      <SectionsMappingProvider>
+        <QueryClientProvider client={queryClient}>
+          <PlanCreation
+            locale={locale}
+            currentOrgId={currentOrgId}
+            currentOrgName={currentOrgName}
+          />
+        </QueryClientProvider>
+      </SectionsMappingProvider>
     </Global>
   );
 }
