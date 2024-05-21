a538813d0f3614efca5f36d750992f6c9ae8d5b7
Hugo COLLIN <hugo.collin@inist.fr>
2024-05-21 16:28:50 +0200

    chore: Adding new form (in anticipation for templates selection)

--- src/components/TemplateMapping/TemplateMapping.jsx
+++ src/components/TemplateMapping/TemplateMapping.jsx
@@ -3,13 +3,16 @@ import { useTranslation } from "react-i18next";
 import SectionsContent from "../Shared/SectionsContent/SectionsContent";
 import { GlobalContext } from "../context/Global";
 import useSectionsMapping from "../../hooks/useSectionsMapping";
+import SelectSingleList from "../FormComponents/SelectSingleList";
+import { FormProvider, useForm } from "react-hook-form";
 
 
-function TemplateMapping({ locale, initialTemplateId, targetTemplateId }) {
+function TemplateMapping({ data, locale, initialTemplateId, targetTemplateId }) {
   // --- STATE ---
   const { i18n } = useTranslation();
   const { setLocale } = useContext(GlobalContext);
   const { enableMapping, USAGE_INITIAL, USAGE_TARGET } = useSectionsMapping();
+  const methods = useForm({ defaultValues: data });
   const targetRef = useRef(null); 
   
   const INNER_SCROLLING_DEFAULT_HEIGHT = "calc(100vh - 100px)";
@@ -20,7 +23,7 @@ function TemplateMapping({ locale, initialTemplateId, targetTemplateId }) {
     height: height,
     maxHeight: INNER_SCROLLING_DEFAULT_HEIGHT,
     overflowY: "auto",
-    marginTop: "10vh",
+    marginTop: "100px",
     padding: "0 1em",
   };
 
@@ -44,7 +47,7 @@ function TemplateMapping({ locale, initialTemplateId, targetTemplateId }) {
     if (targetRef.current) {
       const rect = targetRef.current.getBoundingClientRect();
       const distanceFromTop = rect.top;
-      const calculatedHeight = `calc(100vh - ${distanceFromTop}px)`;
+      const calculatedHeight = `calc(100vh - ${distanceFromTop + 100}px)`;
       setHeight(calculatedHeight);
     }
   };
@@ -54,18 +57,43 @@ function TemplateMapping({ locale, initialTemplateId, targetTemplateId }) {
     <>
       <h1>Template Mapping</h1>
       <p>Here you can map the sections of the initial template to the sections of the target template.</p>
+      <div className="row">
+        <FormProvider {...methods} >
+          <div className="col-md-6">
+            <h2>Initial Template</h2>
+            {/* <SelectSingleList
+              key={"bla"}
+              label={"test"}
+              formLabel={"test0"}
+              propName={"bla"}
+              // tooltip={tooltip}
+              // registries={registries}
+              // registryType={registryType}
+              // templateName={prop.template_name || prop.items?.template_name}
+              defaultValue={"rrrrr"}
+              // overridable={prop["overridable"]}
+              // readonly={false}
+            /> */}
+          </div>
+          <div ref={targetRef} className="col-md-6">
+            <h2>Target Template</h2>
+
+          </div>
+        </FormProvider>
+      </div>
       <div className="row" style={{
         height: height,
         maxHeight: INNER_SCROLLING_DEFAULT_HEIGHT,
       }}>
         <div className="col-md-6">
-          <h2>Initial Template</h2>
+          {/* <h2>Initial Template</h2>
+          <SelectSingleList /> */}
           <div style={innerScrollingFormsStyle}>
             <SectionsContent templateId={initialTemplateId} readonly id='left' mappingUsage={USAGE_INITIAL} />
           </div>
         </div>
         <div ref={targetRef} className="col-md-6">
-          <h2>Target Template</h2>
+          {/* <h2>Target Template</h2> */}
           <div style={{...innerScrollingFormsStyle, right: "0"}}>
             <SectionsContent templateId={targetTemplateId} id='right' hiddenFields mappingUsage={USAGE_TARGET} />
           </div>
