2c043709df3e1d3838095c616b7572c9aa64cc94
Hugo COLLIN <hugo.collin@inist.fr>
2024-06-05 11:34:58 +0200

    feat: sorting on columns

--- src/components/TemplateMapping/TemplateMappingsList.jsx
+++ src/components/TemplateMapping/TemplateMappingsList.jsx
@@ -5,6 +5,11 @@ import { capitalizeFirstLetter } from '../../utils/utils.js';
 
 function TemplateMappingsListLayout() {
   const [mappings, setMappings] = useState([]);
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: '',
+    icon: 'fas fa-sort'
+  });
 
   const { getMappings } = useSectionsMapping();
 
@@ -22,17 +27,56 @@ function TemplateMappingsListLayout() {
     window.location.href = `/super_admin/template_mappings/${id}/edit`;
   };
 
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    let icon = 'fas fa-sort-up';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+      icon = 'fas fa-sort-down';
+    } else if (sortConfig.key === key && sortConfig.direction === 'descending') {
+      direction = '';
+      icon = 'fas fa-sort';
+    }
+    setSortConfig({ key, direction, icon });
+    sortArray(key, direction);
+  };
+
+  const sortArray = (key, direction) => {
+    let sortedMappings = [...mappings];
+    if (direction !== '') {
+      sortedMappings.sort((a, b) => {
+        if (a[key] < b[key]) {
+          return direction === 'ascending' ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+          return direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    } else {
+      sortedMappings = [...mappings]; // Reset to original
+    }
+    setMappings(sortedMappings);
+  };
+
   return (
     <div className="table-responsive">
       <table className="table table-hover">
         <thead>
           <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Mapping Type</th>
-            <th scope="col">Source ID</th>
-            <th scope="col">Target ID</th>
-            <th scope="col">Created At</th>
-            <th scope="col">Updated At</th>
+            {['id', 'type_mapping', 'source_id', 'target_id', 'created_at', 'updated_at'].map((key) => (
+              <th key={key} scope="col" onClick={() => requestSort(key)}>
+                {capitalizeFirstLetter(key.replace('_', ' '))}
+                <i className={key === sortConfig.key ? sortConfig.icon : 'fas fa-sort'} aria-hidden="true" style={{
+                  float: "right", 
+                  fontSize: "1.2em",
+                }}>
+                  <span className="screen-reader-text">
+                    {/* TODO: screen reader text */}
+                  </span>
+                </i>
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
