ce37b9af599a1b82fcc165646d44f7730de86095
Hugo COLLIN <hugo.collin@inist.fr>
2024-06-05 17:02:06 +0200

    refactor: cleaning code

--- src/components/TemplateMapping/TemplateMappingsListLayout.jsx
+++ src/components/TemplateMapping/TemplateMappingsListLayout.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-
-import Global from '../context/Global.jsx';
 import '../../i18n.js';
 
 import TemplateMappingsList from './TemplateMappingsList.jsx';
 import { SectionsMappingProvider } from '../context/SectionsMappingContext.jsx';
-import { TemplateProvider } from '../context/TemplateContext.jsx';
 
 const toastOptions = {
   duration: 5000,
@@ -17,16 +14,14 @@ function TemplateMappingsListLayout({
   mappingId,
 }) {
   return (
-    // <Global>
-        <SectionsMappingProvider>
-          <TemplateMappingsList
-            locale={locale}
-            mappingId={mappingId}
-            readonly
-          />
-          <Toaster position="top-center" toastOptions={toastOptions} reverseOrder={false} />
-        </SectionsMappingProvider>
-    // </Global>
+    <SectionsMappingProvider>
+      <TemplateMappingsList
+        locale={locale}
+        mappingId={mappingId}
+        readonly
+      />
+      <Toaster position="top-center" toastOptions={toastOptions} reverseOrder={false} />
+    </SectionsMappingProvider>
   )
 }
 
