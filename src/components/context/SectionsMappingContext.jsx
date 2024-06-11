4070932c48bd3400340d705b81fd07c9ee39137b
Hugo COLLIN <hugo.collin@inist.fr>
2024-06-11 15:40:31 +0200

    style: improving UX

--- src/components/context/SectionsMappingContext.jsx
+++ src/components/context/SectionsMappingContext.jsx
@@ -193,16 +193,10 @@ export const SectionsMappingProvider = ({ children }) => {
       text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
       confirmButtonText: 'Yes, delete it!'
     })
-    // .then((result) => {
-    //   if (result.isConfirmed) {
-    //     deleteMapping();
-    //     confirmationModal();
-    //   }
-    // });
   };
 
   const confirmationModal = async () => {
