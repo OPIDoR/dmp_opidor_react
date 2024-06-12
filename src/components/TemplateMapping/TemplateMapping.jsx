4f79bfc01a8c42f1b38fa87c2983d404bd10b8fb
Hugo COLLIN <hugo.collin@inist.fr>
2024-06-12 11:49:21 +0200

    feat: floating page content heading

--- src/components/TemplateMapping/TemplateMapping.jsx
+++ src/components/TemplateMapping/TemplateMapping.jsx
@@ -94,26 +94,33 @@ function TemplateMapping({ data, locale, mappingId }) {
 
 export default TemplateMapping;
 
+function ContentHeading({ templateMappingId, saveMapping }) {
+  const stickyStyle = {
+    position: "sticky",
+    top: "60px", // Replace with navbar height constant
+    zIndex: 1000,
+    backgroundColor: "white",
+    width: "100%",
+  };
 
-function ContentHeading({templateMappingId, saveMapping}) {
-  return <div className="row">
-    <div className="col-md-12">
-      <h1 style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          <a href="/super_admin/template_mappings"
-            className="btn btn-primary pull-left">{t('← All Mappings')}</a>
-        </div>
-        <span>{(templateMappingId ? `${t('Edit Mapping')} ${templateMappingId}` : t('Create Mapping'))}</span>
-        <div>
-          <div style={{ visibility: templateMappingId ? 'visible' : 'hidden' }}>
-            <CustomButton
-              title="💾 Save mapping"
-              handleClick={saveMapping}
-              buttonColor="orange" />
+  return (
+    <div className="row content-heading" style={stickyStyle}>
+      <div className="col-md-12">
+        <h1 style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <a href="/super_admin/template_mappings" className="btn btn-primary pull-left">{t('← All Mappings')}</a>
           </div>
-        </div>
-      </h1>
+          <span>{(templateMappingId ? `${t('Edit Mapping')} ${templateMappingId}` : t('Create Mapping'))}</span>
+          <div>
+            <div style={{ visibility: templateMappingId ? 'visible' : 'hidden' }}>
+              <CustomButton
+                title="💾 Save mapping"
+                handleClick={saveMapping}
+                buttonColor="orange" />
+            </div>
+          </div>
+        </h1>
+      </div>
     </div>
-  </div>;
+  );
 }
-
