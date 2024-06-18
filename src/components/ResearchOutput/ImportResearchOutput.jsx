17e0221b074a63f35f277ae18cbf67e7ce7c14ad
wilmouths <wilmouthsteven@gmail.com>
2024-06-18 16:06:39 +0200

    feat: adding research output import

--- src/components/ResearchOutput/ImportResearchOutput.jsx
+++ src/components/ResearchOutput/ImportResearchOutput.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Button } from "react-bootstrap";
+import toast from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import * as stylesForm from "../assets/css/form.module.css";
 import { GlobalContext } from "../context/Global";
@@ -12,8 +13,13 @@ const EndButton = styled.div`
   justify-content: end;
 `;
 
-function ImportResearchOutput({ planId, handleClose, show }) {
-  const { setResearchOutputs } = useContext(GlobalContext);
+function ImportResearchOutput({ planId, handleClose }) {
+  const {
+    researchOutputs, setResearchOutputs,
+    setDisplayedResearchOutput,
+    setLoadedSectionsData,
+    setUrlParams,
+  } = useContext(GlobalContext);
   const { t } = useTranslation();
   const [plans, setPlans] = useState([]);
   const [selectedPlan, setSelectedPlan] = useState({});
@@ -24,9 +30,11 @@ function ImportResearchOutput({ planId, handleClose, show }) {
       const plans = data?.plans?.map((plan) => ({
         value: plan.id,
         label: plan.title,
+        ...plan,
         researchOutputs: plan.research_outputs.map((ro) => ({
           value: ro.id,
           label: ro.title,
+          ...ro,
         })),
       }));
       setPlans(plans || []);
@@ -52,12 +60,31 @@ function ImportResearchOutput({ planId, handleClose, show }) {
   /**
    * This function handles the import of a product plan and updates the product data.
    */
-  const handleImportPlan = (e) => {
+  const handleImportPlan = async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    researchOutput.postImportProduct(selectedPlan.id, selectedResearchOutput.id).then((res) => {
-      console.log(res);
-    });
+
+    let res;
+    try {
+      res = await researchOutput.postImportProduct(  {
+        planId,
+        uuid: selectedResearchOutput.uuid,
+      });
+    } catch (err) {
+      console.log(err);
+      return toast.error(t('An error occured during import !'));
+    }
+
+    const { research_outputs, current_research_output } = res?.data?.data;
+
+    const currentResearchOutput = research_outputs.find(({ id }) => id === current_research_output);
+
+    setResearchOutputs(research_outputs);
+    setDisplayedResearchOutput(currentResearchOutput);
+    setLoadedSectionsData({ [currentResearchOutput.template.id]: currentResearchOutput.template })
+    setUrlParams({ research_output: current_research_output });
+
+    return toast.success(res?.data?.message);
   };
 
   return (
