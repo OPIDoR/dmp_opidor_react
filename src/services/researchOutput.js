df8a7e149498c13d6c568f60f21feb93086bc19c
Benjamin FAURE <benjamin.faure@inist.fr>
2024-06-24 15:31:52 +0200

    refactor: improved research_output import

--- src/services/researchOutput.js
+++ src/services/researchOutput.js
@@ -3,36 +3,6 @@ import createHeaders from '../utils/HeaderBuilder';
 import { toast } from "react-hot-toast";
 import { getErrorMessage } from '../utils/utils';
 
-const plans = [
-  {
-    id: 18024,
-    title: 'DMP du projet "Silicium, soufre et carbone issu de biomasse pour des batteries durables"',
-  },
-  {
-    id: 18290,
-    title: 'DMP du projet "Histoire et archéologie des monastères et des sites ecclésiaux d’Istrie et de Dalmatie (IVe-XIIe s.)"',
-  },
-  {
-    id: 14894,
-    title: 'DMP du projet "Complexes de lanthanides luminescents avec réponse optique dynamique ajustable"',
-  },
-];
-
-const products = [
-  {
-    uuid: "65856096-32be-4dc0-ad47-841848709c93",
-    title: "Default",
-  },
-  {
-    uuid: "ed0cff2c-1f2e-4f0c-ac7b-09d16d472a2c",
-    title: "Research Output 2",
-  },
-  {
-    uuid: "dfb77c60-0040-4259-81d6-223f146a9f21",
-    title: "Research Output 3",
-  },
-];
-
 /**
  * This function sends a post request to a server with a JSON object and handles any errors that may occur.
  * @param jsonObject - This is an object containing the data to be sent in the POST request.
@@ -51,7 +21,7 @@ const get = async (roId) => axios.get(`/research_outputs/${roId}`);
  * @param researchOutputId - The researchOutputId parameter is a unique identifier for a product that is being imported.
  * @returns an object with a "data" property that contains the updated data stored in the session storage.
  */
-const postImportProduct = async ({ planId, uuid }) => {
+const importResearchOutput = async ({ planId, uuid }) => {
   let res;
   try {
     res = await axios.post(`/research_outputs/import?plan_id=${planId}`, { uuid }, { headers: createHeaders({}, true)});
@@ -101,7 +71,7 @@ export default {
   create,
   update,
   get,
-  postImportProduct,
+  importResearchOutput,
   deleteResearchOutput,
   getPlans,
 };
