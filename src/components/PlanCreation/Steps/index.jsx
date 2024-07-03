4bfa847a5df2a68666f4d54165df950506da1c01
wilmouths <wilmouthsteven@gmail.com>
2024-07-03 14:52:19 +0200

    feat: import dmp

--- src/components/PlanCreation/Steps/index.jsx
+++ src/components/PlanCreation/Steps/index.jsx
@@ -1,9 +1,15 @@
+import ActionSelection from "./ActionSelection";
 import ContextSelection from "./ContextSelection";
 import TemplateSelection from "./TemplateSelection";
+import FormatSelection from "./FormatSelection";
 import LangSelection from "./LangSelection";
+import Import from "./Import";
 
 export {
+  ActionSelection,
   ContextSelection,
   TemplateSelection,
+  FormatSelection,
   LangSelection,
+  Import,
 };
