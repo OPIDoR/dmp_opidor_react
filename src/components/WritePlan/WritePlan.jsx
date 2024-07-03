5a3fdefba529e2f6d6138ce02dfa21a444c72d37
wilmouths <wilmouthsteven@gmail.com>
2024-07-03 15:47:50 +0200

    fix: removing null on handleClose

--- src/components/WritePlan/WritePlan.jsx
+++ src/components/WritePlan/WritePlan.jsx
@@ -137,10 +137,10 @@ function WritePlan({
               <div style={{ justifyContent: 'center', alignItems: 'center', left: 0 }}>
                 <Tabs className={`mb-3 ${modalStyles.modal_tabs}`} defaultActiveKey={"create"} id="create-edit-research-output-tabs">
                   <Tab eventKey={"create"} title={t("Create")}>
-                    <AddResearchOutput planId={planId} handleClose={null} close={false} show={true} edit={false} />
+                    <AddResearchOutput planId={planId} handleClose={() => {}} close={false} show={true} edit={false} />
                   </Tab>
                   <Tab eventKey="import" title={t("Import")}>
-                    <ImportResearchOutput planId={planId} handleClose={null}  close={false} show={true} />
+                    <ImportResearchOutput planId={planId} handleClose={() => {}} close={false} show={true} />
                   </Tab>
                 </Tabs>
               </div>
