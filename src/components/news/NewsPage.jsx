abebd890ec08bbb2fa7409242e93cc5337e82a0d
Benjamin FAURE <benjamin.faure@inist.fr>
2024-07-04 15:56:02 +0200

    refactor: migrated news page to bootstrap 5

--- src/components/news/NewsPage.jsx
+++ src/components/news/NewsPage.jsx
@@ -3,7 +3,12 @@ import { useQuery } from 'react-query';
 import { format } from 'date-fns';
 import { fr, enGB } from 'date-fns/locale';
 import { useTranslation } from 'react-i18next';
-import { Row, Col, Thumbnail, Button } from 'react-bootstrap';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 import CustomSpinner from '../Shared/CustomSpinner.jsx';
 import CustomError from '../Shared/CustomError.jsx';
 import { news } from '../../services';
@@ -25,10 +30,7 @@ export default function NewsPage({ locale, size }) {
   if (error) return <CustomError error={error} />;
 
   return (
-    <Row style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-    }}>
+    <Row xs={1} md={3} className="g-4">
       {data.map((r) => ({
         id: r.id,
         title: r.title.rendered,
@@ -36,48 +38,32 @@ export default function NewsPage({ locale, size }) {
         link: r.link,
         date: format(new Date(r.date), 'dd/MM/yyyy', { locale: locales[i18n.resolvedLanguage || locale] }),
         full: r?.["_embedded"]?.["wp:featuredmedia"]?.[0]?.["media_details"]?.["sizes"]?.["full"],
-      })).map((n, id) => (
-        <Col
-          xs={12}
-          md={4}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-          key={`col-${n.id}`}
-        >
-          <Thumbnail
-            src={n.full.source_url}
-            style={{
-              flex: 1,
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-            key={`thumbnail-${n.id}`}
-          >
-            <h2 key={`title-${n.id}`}>
-              <a key={`link-${n.id}`} href={n.link} dangerouslySetInnerHTML={{ __html: n.title }} />
-            </h2>
-            <p
-              style={{
-                marginBottom: '50px',
-              }}
-              dangerouslySetInnerHTML={{ __html: `${n.content.substring(0, 120)}...` }}
-              key={`content-${n.id}`}
-            />
-            <p>
+      })).map((n, idx) => (
+        <Col key={idx}>
+          <Card
+            style={{ padding: '5px', margin: '0 15px', borderColor: '#ddd', height: '100%' }}
+            key={`thumbnail-${n.id}`}>
+            <Card.Img variant="top" src={n.full.source_url} />
+            <Card.Body>
+              <Card.Title as="h2">
+                <Card.Link style={{ backgroundColor: 'white', textDecoration: 'none' }} key={`link-${n.id}`} href={n.link}>{n.title}</Card.Link>
+              </Card.Title>
+              <Card.Text
+                style={{
+                  marginBottom: '50px',
+                }}
+                dangerouslySetInnerHTML={{ __html: `${n.content.substring(0, 120)}...` }}
+                key={`content-${n.id}`} />
+            </Card.Body>
+            <Card.Footer className="text-muted" style={{ border: 'none', backgroundColor: 'white'}}>
               <Button
                 href={n.link}
-                bsStyle="primary"
-                style={{
-                  position: 'absolute',
-                  bottom: '20px',
-                  right: '30px',
-                }}
+                variant="primary"
                 key={`read-button-${n.id}`}
+                className="float-end"
               >{t('Read article')}</Button>
-            </p>
-          </Thumbnail>
+            </Card.Footer>
+          </Card>
         </Col>
       ))}
     </Row>
