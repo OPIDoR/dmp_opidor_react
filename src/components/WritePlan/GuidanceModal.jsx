51c25f2ce51f2b374b5f1274d750c9799e77eb0e
wilmouths <wilmouthsteven@gmail.com>
2024-07-30 13:36:07 +0200

    fix: displaying all guidances

--- src/components/WritePlan/GuidanceModal.jsx
+++ src/components/WritePlan/GuidanceModal.jsx
@@ -23,7 +23,6 @@ function GuidanceModal({ show, setShowGuidanceModal, setFillColorGuidanceIcon, q
   const [error, setError] = useState(null);
   const [indexTab, setIndexTab] = useState(0);
   const modalRef = useRef(null);
-  const [guidancesGroups, setGuidancesGroups] = useState({});
 
   const {
     planInformations,
@@ -57,20 +56,25 @@ function GuidanceModal({ show, setShowGuidanceModal, setFillColorGuidanceIcon, q
     if (!questionsWithGuidance.includes(questionId)) { return; }
 
     setLoading(true);
-    guidances.getGuidanceGroups(planId, locale)
-      .then((res) => setGuidancesGroups(
-        res?.data?.data?.flatMap((groups) => groups.guidance_groups.flatMap((group) => group))
-          ?.reduce((prev, curr) => ({ ...prev, [curr.id]: curr.name }), {}),
-      ))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
 
     guidances.getGuidances(planId, questionId)
       .then(({ data }) => {
-        const guidancesData = data?.guidances;
+        const guidancesData = data?.guidances.map(guidance => {
+          const groups = guidance.groups.reduce((acc, group) => {
+            const [groupInfo, guidanceInfo] = group;
+            const groupName = groupInfo.name;
+            acc[groupName] = guidanceInfo['Data Description'];
+            return acc;
+          }, {});
+          return {
+            name: guidance.name,
+            title: 'Data Description',
+            groups,
+            annotations: guidance.annotations
+          };
+        });
         setData(guidancesData);
-        const activetab = guidancesData?.[0]?.name || '';
-        setActiveTab(activetab);
+        setActiveTab(guidancesData?.[0]?.name || '');
       })
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
@@ -99,18 +103,20 @@ function GuidanceModal({ show, setShowGuidanceModal, setFillColorGuidanceIcon, q
                 ))}
               </>
             )}
-            {Object.keys(data?.[indexTab]?.groups).map((theme, themeId) => (
-              <div key={`guidance-theme-${themeId}`}>
-                {themeId === 0 && <Theme alt={theme}>{theme}</Theme>}
-                {data?.[indexTab]?.groups?.[theme]?.filter(({ locale }) => locale === locales[planInformations.locale])?.map((g, id) => (
-                  <div key={`guidance-theme-${themeId}-id-${id}-content`}>
-                    {id == 0 && <SubTitle>{guidancesGroups[g.guidance_group_id]}</SubTitle>}
-                    <div
-                      key={`guidance-theme-${themeId}-id-${id}`}
-                      dangerouslySetInnerHTML={{
-                        __html: DOMPurify.sanitize(g.text),
-                      }}
-                    />
+            {data?.filter(({ name }) => name === activeTab).map(({ title, groups }, dId) => (
+              <div key={`guidance-${dId}`}>
+                <Theme alt={title}>{title}</Theme>
+                {Object.entries(groups).map(([key, guidances]) => (
+                  <div>
+                    <SubTitle key={`guidance-subtitle-${dId}`}>{key}</SubTitle>
+                    {guidances.map((guidance) => (
+                        <div
+                        key={`guidance-value-${dId}`}
+                        dangerouslySetInnerHTML={{
+                          __html: DOMPurify.sanitize(guidance.text),
+                        }}
+                      />
+                    ))}
                   </div>
                 ))}
               </div>
