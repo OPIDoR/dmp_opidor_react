8b58828bea5b56df7b45984be65d47779e83858f
Benjamin FAURE <benjamin.faure@inist.fr>
2025-01-06 15:58:52 +0100

    refactor: added RO description to first RO & changes to research output form CSS

--- src/components/ResearchOutput/ResearchOutputForm.jsx
+++ src/components/ResearchOutput/ResearchOutputForm.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Tab } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Tabs } from "react-bootstrap";
+import DOMPurify from "dompurify";
+
 import AddResearchOutput from "./AddResearchOutput";
 import ImportResearchOutput from "./ImportResearchOutput";
 import * as styles from "../assets/css/modal.module.css";
@@ -16,20 +18,35 @@ function ResearchOutputForm({ planId, handleClose, edit = false }) {
   const { configuration } = useContext(GlobalContext);
 
   return (
-    <>
+    <div className="dmpopidor-branding">
+      <div className={`col-md-12 ${styles.info_box}`}>
+        <fieldset>
+          <legend className={styles.legend}>
+            Info
+          </legend>
+          <div
+            className="col-md-12"
+            style={{ margin: 0, wordWrap: 'break-word' }}
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize([t('Research product: dataset, software, workflow, sample, protocol, etc.<br />The creation of a research product allows you to describe the specific management of this product according to its nature or discipline.<br />You can create as many research products as you need.')]),
+            }}
+          >
+          </div>
+        </fieldset>
+      </div>
       {edit ? (
         <AddResearchOutput planId={planId} handleClose={handleClose} inEdition={edit} />
       ) : (
-        <Tabs className={`mb-3 ${styles.modal_tabs}`} defaultActiveKey={"create"} id="create-edit-research-output-tabs">
-          <Tab eventKey={"create"} title={t("Create")}>
+        <Tabs className={`mb-3 ${styles.modal_tabs}`} defaultActiveKey={"create"}  id="create-edit-research-output-tabs">
+          <Tab eventKey={"create"} title={t("Create")} tabClassName="toto">
             <AddResearchOutput planId={planId} handleClose={handleClose} inEdition={edit} />
           </Tab>
-          {configuration.enableImportResearchOutput && (<Tab eventKey="import" title={t("Import")}>
+          {configuration.enableImportResearchOutput && (<Tab eventKey="import" title={t("Import")} tabClassName="toto">
             <ImportResearchOutput planId={planId} handleClose={handleClose} />
           </Tab>)}
         </Tabs>
       )}
-    </>
+    </div>
   );
 }
 
