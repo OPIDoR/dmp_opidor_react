cdad199c43022a5d7063519df1cc58fe662ef56d
Benjamin FAURE <benjamin.faure@inist.fr>
2025-03-24 09:11:24 +0100

    refactor: added placeholders for WritePlan tab loading

---
+++ src/components/WritePlan/Placeholders/SidebarPlaceholder.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Placeholder from "react-bootstrap/Placeholder";
+
+
+
+function SidebarPlaceholder({ }) {
+
+  const roPlaceholders = [];
+
+  [...Array(4).keys()].forEach((i) => {
+    roPlaceholders.push(
+      <Placeholder key={i} as={Card} animation="wave" style={{ border: 'none', margin: '10px', height: '50px' }}>
+        <Placeholder xs={12} style={{ height: '100%' }} />
+      </Placeholder>
+    )
+  })
+
+  return (
+    <div className="col-2" style={{ padding: '10px' }}>
+      {roPlaceholders}
+    </div>
+  )
+}
+
+export default SidebarPlaceholder;
