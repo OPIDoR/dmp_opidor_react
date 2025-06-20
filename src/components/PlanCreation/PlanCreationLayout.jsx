68ab077d93b66c15730c16b3cd3d2720487b3d27
wilmouths <wilmouthsteven@gmail.com>
2025-06-20 13:23:42 +0200

    fix: clear local storage beforeunload

--- src/components/PlanCreation/PlanCreationLayout.jsx
+++ src/components/PlanCreation/PlanCreationLayout.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Global from '../context/Global.jsx';
 import PlanCreation from './PlanCreation.jsx';
 import '../../i18n';
+import { clearLocalStorage } from '../../utils/utils';
 
 const queryClient = new QueryClient();
 
 function PlanCreationLayout({ locale }) {
+  useEffect(() => {
+    window.addEventListener('beforeunload', () => clearLocalStorage());
+  }, []);
+
   return (
     <Global>
       <QueryClientProvider client={queryClient}>
