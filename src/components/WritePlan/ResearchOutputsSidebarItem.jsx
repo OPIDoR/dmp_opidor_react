0ee0258f3a2244be53d2155d330081d4b5a3b7c5
Benjamin FAURE <benjamin.faure@inist.fr>
2025-08-01 13:15:23 +0000

    feat: adding sort/reorder research outputs

--- src/components/WritePlan/ResearchOutputsSidebarItem.jsx
+++ src/components/WritePlan/ResearchOutputsSidebarItem.jsx
@@ -1,19 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 
-import Nav from "react-bootstrap/Nav";
-
 import { GlobalContext } from "../context/Global";
 import { researchOutput } from "../../services";
 import { except } from "../../utils/utils";
 
-function ResearchOutputsTabs({  researchOutputs, readonly = false, setLoading, children }) {
+function ResearchOutputsSidebarItem({ item, setLoading, children }) {
   const {
-    displayedResearchOutput, setDisplayedResearchOutput,
+    setDisplayedResearchOutput,
     setUrlParams,
     setQuestionsWithGuidance,
   } = useContext(GlobalContext);
   const [selectedResearchOutputId, setSelectedResearchOutputId] = useState(null);
-
 
   useEffect(() => {
     if (selectedResearchOutputId) {
@@ -26,7 +23,6 @@ function ResearchOutputsTabs({  researchOutputs, readonly = false, setLoading, c
     }
   }, [selectedResearchOutputId])
 
-
   /**
    * When the user clicks on a tab, the function sets the active index to the index of the tab that was clicked, and sets the research id to the id of the
    * tab that was clicked.
@@ -38,24 +34,10 @@ function ResearchOutputsTabs({  researchOutputs, readonly = false, setLoading, c
   };
 
   return (
-    <>
-      {researchOutputs && (
-        <Nav style={{width: '100%'}} activeKey={`ro-${displayedResearchOutput.id}`}>
-          {researchOutputs.map((ro, idx) => (
-            <Nav.Item
-              key={idx}
-              onClick={(e) => handleShowResearchOutputClick(e, ro, idx)}
-            >
-              <Nav.Link eventKey={`ro-${ro.id}`} style={{ padding: '20px 0'}}>{ro.abbreviation}</Nav.Link>
-            </Nav.Item>
-          ))}
-          {!readonly && (
-            <>{ children }</>
-          )}
-        </Nav>
-      )}
-    </>
+    <div onClick={(e) => handleShowResearchOutputClick(e, item, item.id)}>
+      {children}
+    </div>
   );
 }
 
-export default ResearchOutputsTabs;
+export default ResearchOutputsSidebarItem;
